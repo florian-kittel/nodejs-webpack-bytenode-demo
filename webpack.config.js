22f6f5c28abd3479a5109e8f4922d3e6c9b4c5bf
florian-kittel <mail@florian-kittel.de>
2018-11-08 22:14:12 +0100

    feat(bundler): add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.js'
+        ],
+    },
+    module: {
+        rules: [
+            {
+                use: []
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([
+            path.resolve('dist')
+        ]),
+        new webpack.IgnorePlugin(/^pg-native$/),
+        new webpack.IgnorePlugin(/^pg.js.*/),
+    ],
+};
